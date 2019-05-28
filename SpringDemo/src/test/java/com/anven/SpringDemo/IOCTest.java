9b3c3986f6d575335f6ae9c7e24d2fa379944a7a
suhw001 <296480474@qq.com>
2019-05-28 09:21:10 +0800

    增加目录结构注意事项

--- SpringDemo/src/test/java/com/anven/SpringDemo/IOCTest.java
+++ SpringDemo/src/test/java/com/anven/SpringDemo/IOCTest.java
@@ -6,6 +6,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.anven.SpringDemo.service.UserService;
 
+/*
+ * Note: 目录结构最好与新建maven项目时填的目录一致，就不需要修改项目的Build Path
+ * 比如groupId为com.anven,artifactId为SpringDemo,则class顶层目录为com.anven.SpringDemo
+ * 
+ */
 public class IOCTest {
 	@Test
 	public void testAddUser() { 
